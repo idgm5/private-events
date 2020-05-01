c77f1a13bf64b5983b8874be10e0a06c013bb17f
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-05-01 10:16:18 -0500

    testing data for model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  #get 'users/show'
+  #get 'users/create'
+  #get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # user
+  resources :users, only: [:index, :show, :new, :create] do
+    resources :attendances, only: [:index, :edit, :update]
+  end
+
 end
