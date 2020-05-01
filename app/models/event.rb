d769bfddc13fec65a8b15e933a7659f680544168
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-05-01 16:08:24 -0500

    adding validation for model entities

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :attendances, dependent: :destroy
   has_many :guests, through: :attendances, source: :user
+
+  validates :description, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :events_as_creator, foreign_key: :creator_id, class_name: 'Event'
   has_many :attendances
   has_many :events_as_guest, through: :attendances, source: :event
+
+  validates :name, presence: true, uniqueness: true
+
 end
